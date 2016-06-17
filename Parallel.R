78bd527372535ce11cb48171fc527ef9d1661896
Luca Vignali <lvignali@atsweb.it>
2016-06-17 11:02:12 +0200

    First Commit then push

--- Parallel.R
+++ Parallel.R
@@ -1,5 +1,5 @@
 ##### The aim of this script is to load simple-process and save a large file. 
 # in this case 5GB csv, zipped as 515MB file.
-
+# First commit locally, then push to the repository.
 
 
